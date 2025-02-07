b11dd4ce039a292650db088290d8d0fca3ba0428
abhiram-ayyan07 <abhiram612002@gmail.com>
2025-02-08 04:24:23 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [taskList, addTask] = useState([]);
+  const [task, setTask] = useState("");
+  const handleChange = (event)=>{
+    setTask(event.target.value);
+  }
+  const handleTask = ()=>{
+    addTask([...taskList, task]);
+  }
+  const deleteTask = (task)=>{
+    const newList = taskList.filter((taskName)=> task !== taskName);
+    addTask(newList);
+  }
+
   return (
     <div className="App">
+      <div className = "addlist">
+        <input onChange={handleChange}/>
+        <button onClick={handleTask}>add task</button>
+      </div>
+      <div className='list'>
+        {taskList.map((task)=>{
+          return <div><h1>{task}</h1>
+          <button onClick = {()=>deleteTask(task)}>X</button>
+          </div>
+        })}
+      </div>
     </div>
   );
 }
